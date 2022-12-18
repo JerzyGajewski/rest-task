8c425e2774b4ecd591735bc92f96d5c4fd67202a
Jerzy Gajewski <jerzygajewski3@gmail.com>
2022-12-18 14:58:11 +0100

    Created maven project

---
+++ src/test/java/gajewski/jerzy/rest/task/RestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package gajewski.jerzy.rest.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
